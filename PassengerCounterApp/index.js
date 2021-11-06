1028c11ca7187814bc7a6c2ce754491df44c3d43
Sven <svenwu996@gmail.com>
2021-11-05 17:30:27 -0700

    finished first app

--- PassengerCounterApp/index.js
+++ PassengerCounterApp/index.js
@@ -1,16 +1,37 @@
 // document.getElementById("count-el").innerText = 5
 
 let count = 0
+let countEl = document.getElementById("count-el")
+
+// 1. Grab the save-el paragrah and store it in a variable called saveEl
+let saveEL = document.getElementById("save-el")
+
 
 function increment() {
     console.log("Button was clicked")
     count = count+1
-    let countEl = document.getElementById("count-el")
-    countEl.innerText = count
+    // countEl.innerText = count
+    countEl.textContent = count
     console.log(count)
 }
 
 // Create function save() which logs out the count
 function save() {
+    // 2. Create a variable that contains both the count and the dash separator, i.e. "12 - "
+    let saveElText = count + " - "
+
+    // 3. Render the variable in the saveEl using innerText
+    // NB: Make sure to not delete the existing content of the paragraph
+    // saveEL.innerText += saveElText
+
+    //To fix the spacng since innerText can struggle with reading in hidden text
+    saveEL.textContent += saveElText
+
     console.log(count)
+
+    //Reset count to 0
+    countEl.textContent = 0
+    count = 0
 }
+
+console.log("Let's count people on the subway!")
