bf08dd2a08de0126b1ccbbdd1c311754a382ed46
Sven <svenwu996@gmail.com>
2021-11-11 10:13:58 -0800

    pushing arrays on event listener

--- ChromeExtension/LeadsArray/index.js
+++ ChromeExtension/LeadsArray/index.js
@@ -9,7 +9,9 @@ const inputEl = document.getElementById("input-el")
 
 const inputBtn = document.getElementById("input-btn")
 
+// Push the value "www.awesomelead.com" to myArray when the input button is clicked
 inputBtn.addEventListener("click", function() {
+    myLeads.push("www.awesomelead.com")
     console.log("Button clicked!")
 })
 
